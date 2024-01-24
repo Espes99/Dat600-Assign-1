f7487746372040f3b15eb8a7266b214fe1d3f4e4
Espen Sele <espesele@stud.ntnu.no>
2024-01-24 14:55:16 +0100

    git init

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Your main method code goes here
+        Console.WriteLine("Hello, World! :D");
+    }
+
+    static void InsertionSort()
+    {
+        // Insertion sort will be implemented here and called in main
+    }
+}
