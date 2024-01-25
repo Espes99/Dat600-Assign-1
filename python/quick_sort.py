6676636439d6729c00e512ad8481eb8c484291bb
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2024-01-25 19:56:17 +0100

    Modify function to count steps

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -1,10 +1,27 @@
 #Quick sort using comprehension
 #ref: https://www.geeksforgeeks.org/python-program-for-quicksort/
 def quick_sort(arr):
+    steps = 0
     if len(arr) <= 1:
-        return arr
+        return steps
     else:
         pivot = arr[0]
-        left = [x for x in arr[1:] if x < pivot]
-        right = [x for x in arr[1:] if x >= pivot]
-        return quick_sort(left) + [pivot] + quick_sort(right)
+        left = []
+        right = []
+        steps += 1  
+
+        for x in arr[1:]:
+            steps += 1 
+            if x < pivot:
+                left.append(x)
+            else:
+                right.append(x)
+
+
+        steps += quick_sort(left)
+        steps += quick_sort(right)
+
+
+        steps += len(left) + len(right) + 1
+        
+        return steps
